b221596c839ac60057242334bcafe87dba3e3581
GALMelo <gabriel.aflins@gmail.com>
2021-12-14 16:12:51 -0300

    update

---
+++ ormconfig.js
@@ -0,0 +1,15 @@
+var env = require('dotenv').config();
+
+module.exports = {
+  type: 'mysql',
+  host: env.DATABASE_HOST,
+  port: 3306,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: ['src/*/*.entity{.ts,.js}'],
+  seeds: ['src/seeds/**/*{.ts,.js}'],
+  factories: ['src/factories/**/*{.ts,.js}'],
+  synchronize: true,
+  migrationsTableName: 'custom_migration_table',
+};
