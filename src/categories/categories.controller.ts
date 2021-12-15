3642b2ebed6502de72ee0902e4a1e579f545e732
GALMelo <gabriel.aflins@gmail.com>
2021-12-15 16:43:16 -0300

    update

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -9,7 +9,6 @@ import {
 } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from '../dto/create-category.dto';
-import { UpdateCategoryDto } from '../dto/update-category.dto';
 
 @Controller('categories')
 export class CategoriesController {
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,39 +1,10 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { getRepository } from 'typeorm';
 import { CreateProductDto } from '../dto/create-product.dto';
-import { UpdateProductDto } from '../dto/update-product.dto';
-import { Category } from '../entities/category.entity';
 import { Product } from '../entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  async create(product: CreateProductDto) {
-    try {
-      const newProduct = getRepository(Product).create();
-
-      const category = await getRepository(Category).findOne({
-        id: product.categoryId,
-      });
-
-      newProduct.category = category;
-      newProduct.description = product.description;
-      newProduct.price = product.price;
-      newProduct.qty = product.qty;
-
-      const saveProd = await getRepository(Product).save(newProduct);
-
-      return saveProd;
-    } catch (error) {
-      throw new HttpException(
-        {
-          status: HttpStatus.FORBIDDEN,
-          error: error.message,
-        },
-        HttpStatus.FORBIDDEN,
-      );
-    }
-  }
-
   async findAll() {
     try {
       const result = await getRepository(Product)
@@ -50,17 +21,5 @@ export class ProductsService {
         HttpStatus.FORBIDDEN,
       );
     }
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
-  }
-
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} product`;
   }
 }
