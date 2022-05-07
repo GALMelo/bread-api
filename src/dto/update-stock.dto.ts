51eb9481866cad5964fba7bde689205606321fac
GALMelo <gabriel.aflins@gmail.com>
2022-05-07 17:34:11 -0300

    update UpdateStockDto

--- src/dto/update-stock.dto.ts
+++ src/dto/update-stock.dto.ts
@@ -1,4 +1,19 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateStockDto } from './create-stock.dto';
 
-export class UpdateStockDto extends PartialType(CreateStockDto) {}
+export class UpdateStockDto extends PartialType(CreateStockDto) {
+  @ApiProperty({ type: Number })
+  category_stock_id: number;
+
+  @ApiProperty({ type: String })
+  name: string;
+
+  @ApiProperty({ type: Number })
+  qty?: number;
+
+  @ApiProperty({ type: Date })
+  dt_last_buy: Date;
+
+  @ApiProperty({ type: Number })
+  last_buy_qty: number;
+}
